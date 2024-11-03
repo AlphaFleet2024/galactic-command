05066496f0fa63b15faf91716e4b613038a94b6f
AlphaFleet2024 <d_ehope@outlook.com>
2024-11-03 19:23:42 -0330

    Create About.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Galactic Command</h1>
+            <p>This game allows you to build fleets, gather resources, and engage in battles!</p>
+        </div>
+    );
+};
+
+export default About;
