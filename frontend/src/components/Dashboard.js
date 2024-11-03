41739dbcc763b0de2203e235b308f78cf3e85142
AlphaFleet2024 <d_ehope@outlook.com>
2024-11-03 19:20:40 -0330

    Create Dashboard.js

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ShipList from './ShipList';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Your Dashboard</h1>
+            <ShipList />
+        </div>
+    );
+};
+
+export default Dashboard;
